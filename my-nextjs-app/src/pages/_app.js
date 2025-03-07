860f786003224b620d89d5642496d64c8a927fbe
impulse <loveandsweetsongs@gmail.com>
2025-03-07 17:49:54 +0100

    my commit

---
+++ my-nextjs-app/src/pages/_app.js
@@ -0,0 +1,9 @@
+// filepath: c:\Users\Hp\Desktop\YG FOLDER\code\status checker\my-nextjs-app\src\pages\_app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ my-nextjs-app/src/pages/api/Home.js
@@ -0,0 +1,13 @@
+// filepath: my-nextjs-app/pages/index.js
+import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+
+const Home = () => (
+  <Container>
+    <Jumbotron>
+      <h1>Welcome</h1>
+    </Jumbotron>
+  </Container>
+);
+
+export default Home;
